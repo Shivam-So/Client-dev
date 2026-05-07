0a363f79a2e4bea083a3a77df8097dd2eabd9eea
shivam giri <shivamgiri2501@gmail.com>
2026-05-07 17:24:25 +0530

    debugging 4

--- FormFlow/backend/routes/profileRoutes.js
+++ FormFlow/backend/routes/profileRoutes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const { saveProfile } = require("../controllers/profilecontroller");
+const { saveProfile } = require("../controllers/profileController");
 const auth = require("../middleware/auth");
 
 router.post("/", auth, saveProfile);
 
 module.exports = router;
-
-
